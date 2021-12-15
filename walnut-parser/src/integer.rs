f2bf562a38126c9fe27f9bc2de9e3c8465e81a1b
watcol <potfman@gmail.com>
2021-12-15 14:43:16 +0900

    fix[parser]: Refactorized integer().

--- walnut-parser/src/integer.rs
+++ walnut-parser/src/integer.rs
@@ -1,76 +1,72 @@
-use super::utils::sign;
+use super::utils::{digits, sign};
 use pom::parser::*;
 
 #[allow(dead_code)]
 pub fn integer<'a>() -> Parser<'a, char, i64> {
-    let digit = (one_of("123456789") + one_of("0123456789").repeat(0..))
-        .collect()
-        .convert(|s| s.iter().collect::<String>().parse::<i128>())
-        | sym('0').map(|_| 0);
-    let hex = tag("0x")
-        * one_of("0123456789abcdefABCDEF")
-            .repeat(1..)
-            .convert(|s| i128::from_str_radix(&s.into_iter().collect::<String>(), 16));
-    let oct = tag("0o")
-        * one_of("01234567")
-            .repeat(1..)
-            .convert(|s| i128::from_str_radix(&s.into_iter().collect::<String>(), 8));
-    let bin = tag("0b")
-        * one_of("01")
-            .repeat(1..)
-            .convert(|s| i128::from_str_radix(&s.into_iter().collect::<String>(), 2));
+    let dec = (sign() - digits()).collect().convert(|s| {
+        s.iter()
+            .collect::<String>()
+            .parse::<i64>()
+            .map_err(|_| "Integer too big.")
+    });
+    let hex =
+        (sign() + tag("0x") * one_of("0123456789abcdefABCDEF").repeat(1..)).convert(|(s, i)| {
+            i64::from_str_radix(&s.into_iter().chain(i.into_iter()).collect::<String>(), 16)
+                .map_err(|_| "Integer too big.")
+        });
+    let oct = (sign() + tag("0o") * one_of("01234567").repeat(1..)).convert(|(s, i)| {
+        i64::from_str_radix(&s.into_iter().chain(i.into_iter()).collect::<String>(), 8)
+            .map_err(|_| "Integer too big.")
+    });
+    let bin = (sign() + tag("0b") * one_of("01").repeat(1..)).convert(|(s, i)| {
+        i64::from_str_radix(&s.into_iter().chain(i.into_iter()).collect::<String>(), 2)
+            .map_err(|_| "Integer too big.")
+    });
 
-    (sign() + (hex | oct | bin | digit)).convert(|(s, i)| i64::try_from(s * i))
+    hex | oct | bin | dec
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::test_parser;
+    use crate::{assert_parser, should_fail_parser};
 
     #[test]
-    fn digit() {
-        test_parser!(integer, "0", 0); // Zero
-        test_parser!(integer, "5", 5); // Normal one-digit number
-        test_parser!(integer, "103", 103); // Normal number
-        test_parser!(integer, "+42", 42); // Positive number
-        test_parser!(integer, "-1023456789", -1023456789); // Negative number
-        test_parser!(integer, "9223372036854775807", i64::MAX); // Maximum number.
-        test_parser!(integer, "-9223372036854775808", i64::MIN); // Minimum number.
+    fn dec() {
+        assert_parser!(integer, "0", 0); // Zero
+        should_fail_parser!(integer, "012"); // Value starts with '0'
+        assert_parser!(integer, "5", 5); // Normal one-digit number
+        assert_parser!(integer, "103", 103); // Normal number
+        assert_parser!(integer, "+42", 42); // Positive number
+        assert_parser!(integer, "-1023456789", -1023456789); // Negative number
+
+        // Maximum number.
+        assert_parser!(integer, "9223372036854775807", i64::MAX);
+        should_fail_parser!(integer, "9223372036854775808");
+
+        // Minimum number.
+        assert_parser!(integer, "-9223372036854775808", i64::MIN);
+        should_fail_parser!(integer, "-9223372036854775809");
     }
 
     #[test]
     fn hex() {
-        test_parser!(integer, "0x12", 18); // Normal number
-        test_parser!(integer, "+0x12345abcdef", 0x12345abcdef); // Positive number
-        test_parser!(integer, "-0x67890ABCDEF", -0x67890ABCDEF); // Negative number
-        test_parser!(integer, "0x7fffffffffffffff", i64::MAX); // Maximum number.
-        test_parser!(integer, "-0x8000000000000000", i64::MIN); // Minimum number.
+        assert_parser!(integer, "0x12", 0x12); // Normal number
+        assert_parser!(integer, "+0x01234abcdef", 0x01234abcdef); // Positive number
+        assert_parser!(integer, "-0x56789ABCDEF", -0x56789ABCDEF); // Negative number
     }
 
     #[test]
     fn oct() {
-        test_parser!(integer, "0o12", 10); // Normal number
-        test_parser!(integer, "+0o1234", 0o1234); // Positive number
-        test_parser!(integer, "-0o5670", -0o5670); // Negative number
-        test_parser!(integer, "0o777777777777777777777", i64::MAX); // Maximum number.
-        test_parser!(integer, "-0o1000000000000000000000", i64::MIN); // Minimum number.
+        assert_parser!(integer, "0o12", 0o12); // Normal number
+        assert_parser!(integer, "+0o0123", 0o0123); // Positive number
+        assert_parser!(integer, "-0o4567", -0o4567); // Negative number
     }
 
     #[test]
     fn bin() {
-        test_parser!(integer, "0b10110", 22); // Normal number
-        test_parser!(integer, "+0b101", 5); // Positive number
-        test_parser!(integer, "-0b110", -6); // Negative number
-        test_parser!(
-            integer,
-            "0b111111111111111111111111111111111111111111111111111111111111111",
-            i64::MAX
-        ); // Maximum number.
-        test_parser!(
-            integer,
-            "-0b1000000000000000000000000000000000000000000000000000000000000000",
-            i64::MIN
-        ); // Minimum number.
+        assert_parser!(integer, "0b10110", 0b10110); // Normal number
+        assert_parser!(integer, "+0b101", 0b101); // Positive number
+        assert_parser!(integer, "-0b010", -0b010); // Negative number
     }
 }
--- walnut-parser/src/utils.rs
+++ walnut-parser/src/utils.rs
@@ -1,16 +1,28 @@
 use pom::parser::*;
 
-pub fn sign<'a>() -> Parser<'a, char, i128> {
-    let pos = sym('+').opt().map(|_| 1);
-    let neg = sym('-').map(|_| -1);
-    neg | pos
+pub fn sign<'a>() -> Parser<'a, char, Option<char>> {
+    (sym('+') | sym('-')).opt()
+}
+
+pub fn digits<'a>() -> Parser<'a, char, &'a [char]> {
+    ((one_of("123456789") - one_of("0123456789").repeat(0..)) | (sym('0') - !one_of("0123456789")))
+        .collect()
 }
 
 #[cfg(test)]
 #[macro_export]
-macro_rules! test_parser {
+macro_rules! assert_parser {
     ($parser:expr, $input:expr, $value:expr) => {
         let input: Vec<char> = $input.chars().collect();
         assert_eq!($parser().parse(&input), Ok($value));
+    };
+}
+
+#[cfg(test)]
+#[macro_export]
+macro_rules! should_fail_parser {
+    ($parser:expr, $input:expr) => {
+        let input: Vec<char> = $input.chars().collect();
+        assert!($parser().parse(&input).is_err());
     };
 }
