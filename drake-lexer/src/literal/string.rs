7c9e47d76c084315e11cc39836952e0171fa985a
watcol <potfman@gmail.com>
2022-06-12 18:41:23 +0900

    feat: add characters and strings

--- drake-lexer/src/literal/string.rs
+++ drake-lexer/src/literal/string.rs
@@ -1,32 +1,43 @@
+use somen::error::Expects;
 use somen::prelude::*;
 
-use crate::utils::escaped_char;
+use crate::utils::{escaped_char, escaped_char_continuous, newline};
 
 pub fn character<'a, I>() -> impl Parser<I, Output = char> + 'a
 where
     I: Input<Ok = char> + 'a,
 {
-    escaped_char('\'').between(token('\''), token('\''))
+    escaped_char('\'')
+        .between(token('\''), token('\''))
+        .expect("character")
 }
 
 pub fn string<'a, I>() -> impl Parser<I, Output = String> + 'a
 where
     I: Input<Ok = char> + 'a,
 {
-    choice((newline(), escaped_char('"')))
+    escaped_char_continuous('"')
+        .expect(Expects::from_iter(["character", "continuous line"]))
         .repeat(..)
+        .flatten()
         .collect()
         .between(token('"'), token('"'))
+        .expect("string")
 }
 
-#[inline]
-pub fn newline<'a, I>() -> impl Parser<I, Output = char> + 'a
+pub fn raw_string<'a, I>() -> impl Parser<I, Output = String> + 'a
 where
     I: Input<Ok = char> + 'a,
 {
-    choice((
-        token('\n'),
-        tag("\r\n").map(|_| '\n'),
-        token('\r').map(|_| '\n'),
-    ))
+    token('"')
+        .repeat(3..)
+        .count()
+        .then(|n| {
+            newline()
+                .or(any())
+                .expect("raw character")
+                .until(token('"').times(n).discard())
+        })
+        .collect()
+        .expect("raw string")
 }
--- drake-lexer/src/utils.rs
+++ drake-lexer/src/utils.rs
@@ -8,25 +8,70 @@ pub fn escaped_char<'a, I>(end: char) -> impl Parser<I, Output = char> + 'a
 where
     I: Input<Ok = char> + 'a,
 {
-    none_of([end, '\\']).or(token('\\').prefix(choice((
-        token('n').map(|_| '\n'),
-        token('r').map(|_| '\r'),
-        token('t').map(|_| '\t'),
-        token('\\').map(|_| '\\'),
-        token(end).map(move |_| end),
-        token('x')
-            .prefix(integer_fixed(2, 16, false))
-            .try_map(|b: u8| {
-                if b <= 0x7F {
-                    Ok(b as char)
-                } else {
-                    Err("\\x00 to \\x7F")
-                }
-            }),
-        integer_trailing_zeros(16, false)
-            .try_map(|u: u32| char::from_u32(u).ok_or("valid unicode codepoint"))
-            .between(tag("u{"), token('}')),
-    ))))
+    choice((
+        newline(),
+        none_of([end, '\\']),
+        token('\\').prefix(choice((
+            token('n').map(|_| '\n'),
+            token('r').map(|_| '\r'),
+            token('t').map(|_| '\t'),
+            token('\\').map(|_| '\\'),
+            token(end).map(move |_| end),
+            token('x')
+                .prefix(integer_fixed(2, 16, false))
+                .try_map(|b: u8| {
+                    if b <= 0x7F {
+                        Ok(b as char)
+                    } else {
+                        Err("\\x00 to \\x7F")
+                    }
+                }),
+            integer_trailing_zeros(16, false)
+                .try_map(|u: u32| char::from_u32(u).ok_or("valid unicode codepoint"))
+                .between(tag("u{"), token('}')),
+        ))),
+    ))
+}
+
+pub fn escaped_char_continuous<'a, I>(end: char) -> impl Parser<I, Output = Option<char>> + 'a
+where
+    I: Input<Ok = char> + 'a,
+{
+    choice((
+        newline().map(Some),
+        none_of([end, '\\']).map(Some),
+        token('\\').prefix(choice((
+            token('n').map(|_| Some('\n')),
+            token('r').map(|_| Some('\r')),
+            token('t').map(|_| Some('\t')),
+            token('\\').map(|_| Some('\\')),
+            token(end).map(move |_| Some(end)),
+            token('x')
+                .prefix(integer_fixed(2, 16, false))
+                .try_map(|b: u8| {
+                    if b <= 0x7F {
+                        Ok(Some(b as char))
+                    } else {
+                        Err("\\x00 to \\x7F")
+                    }
+                }),
+            integer_trailing_zeros(16, false)
+                .try_map(|u: u32| char::from_u32(u).ok_or("valid unicode codepoint").map(Some))
+                .between(tag("u{"), token('}')),
+            newline().map(|_| None),
+        ))),
+    ))
+}
+
+pub fn newline<'a, I>() -> impl Parser<I, Output = char> + 'a
+where
+    I: Input<Ok = char> + 'a,
+{
+    choice((
+        token('\n'),
+        tag("\r\n").map(|_| '\n'),
+        token('\r').map(|_| '\n'),
+    ))
 }
 
 #[cfg(test)]
@@ -40,6 +85,7 @@ mod tests {
         block_on(async {
             let parser = &mut super::escaped_char('\'').complete();
             assert_parser(parser, "a", 'a').await;
+            assert_parser(parser, "\n", '\n').await;
             assert_parser(parser, "\\n", '\n').await;
             assert_parser(parser, "\\r", '\r').await;
             assert_parser(parser, "\\t", '\t').await;
@@ -61,6 +107,47 @@ mod tests {
             assert_parser_fail(parser, "\\u{DFFF}").await;
             assert_parser_fail(parser, "\\u{110000}").await;
         });
+    }
+
+    #[test]
+    fn escaped_char_continuous() {
+        block_on(async {
+            let parser = &mut super::escaped_char_continuous('\'').complete();
+            assert_parser(parser, "a", Some('a')).await;
+            assert_parser(parser, "\n", Some('\n')).await;
+            assert_parser(parser, "\\n", Some('\n')).await;
+            assert_parser(parser, "\\r", Some('\r')).await;
+            assert_parser(parser, "\\t", Some('\t')).await;
+            assert_parser(parser, "\\\\", Some('\\')).await;
+            assert_parser(parser, "\\'", Some('\'')).await;
+            assert_parser(parser, "\\x00", Some('\x00')).await;
+            assert_parser(parser, "\\x0A", Some('\x0A')).await;
+            assert_parser(parser, "\\x7f", Some('\x7f')).await;
+            assert_parser(parser, "\\u{Ab}", Some('\u{AB}')).await;
+            assert_parser(parser, "\\u{00a0}", Some('\u{A0}')).await;
+            assert_parser(parser, "\\u{D7FF}", Some('\u{D7FF}')).await;
+            assert_parser(parser, "\\u{E000}", Some('\u{E000}')).await;
+            assert_parser(parser, "\\u{10FFFF}", Some('\u{10FFFF}')).await;
+            assert_parser(parser, "\\\n", None).await;
+            assert_parser_fail(parser, "'").await;
+            assert_parser_fail(parser, "\\").await;
+            assert_parser_fail(parser, "\\x80").await;
+            assert_parser_fail(parser, "\\U{00}").await;
+            assert_parser_fail(parser, "\\u{D800}").await;
+            assert_parser_fail(parser, "\\u{DFFF}").await;
+            assert_parser_fail(parser, "\\u{110000}").await;
+        });
+    }
+
+    #[test]
+    fn newline() {
+        block_on(async {
+            let parser = &mut super::newline().complete();
+            assert_parser(parser, "\n", '\n').await;
+            assert_parser(parser, "\r", '\r').await;
+            assert_parser(parser, "\r\n", '\n').await;
+            assert_parser_fail(parser, "\n\r").await;
+        })
     }
 }
 
