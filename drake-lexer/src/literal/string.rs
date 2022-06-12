3ceeab1a2f84d4a497cb71abbd7b779c5c6550e2
watcol <potfman@gmail.com>
2022-06-12 18:57:30 +0900

    feat(test): add tests for strings

--- drake-lexer/src/literal/string.rs
+++ drake-lexer/src/literal/string.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use somen::error::Expects;
 use somen::prelude::*;
 
---
+++ drake-lexer/src/literal/string/tests.rs
@@ -0,0 +1,35 @@
+use futures_executor::block_on;
+use somen::prelude::*;
+
+use crate::utils::assert_parser;
+
+#[test]
+fn character() {
+    block_on(async {
+        let parser = &mut super::character().complete();
+        assert_parser(parser, "'a'", 'a').await;
+    })
+}
+
+#[test]
+fn string() {
+    block_on(async {
+        let parser = &mut super::string().complete();
+        assert_parser(parser, "\"abc\"", String::from("abc")).await;
+        assert_parser(parser, "\"\\\n\r\n\r\"", String::from("\n\n")).await;
+    })
+}
+
+#[test]
+fn raw_string() {
+    block_on(async {
+        let parser = &mut super::raw_string().complete();
+        assert_parser(
+            parser,
+            "\"\"\"\\ServerX\\admin$\\system32\\\"\"\"",
+            String::from("\\ServerX\\admin$\\system32\\"),
+        )
+        .await;
+        assert_parser(parser, "\"\"\"\"(\"\"\")\"\"\"\"", String::from("(\"\"\")")).await;
+    })
+}
