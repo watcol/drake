d79696fca2f6402f5efae3f928d629069b9e7de2
watcol <potfman@gmail.com>
2022-06-13 09:32:27 +0900

    feat: add symbol lexer

--- drake-lexer/src/lib.rs
+++ drake-lexer/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod literal;
+pub mod symbol;
 mod utils;
---
+++ drake-lexer/src/symbol.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod tests;
+
+use somen::prelude::*;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Symbol {
+    Assign,
+    Comma,
+    Dot,
+    OpenBracket,
+    CloseBracket,
+    OpenBrace,
+    CloseBrace,
+}
+
+pub fn symbol<'a, I>() -> impl Parser<I, Output = Symbol> + 'a
+where
+    I: Input<Ok = char> + 'a,
+{
+    choice((
+        token('=').map(|_| Symbol::Assign),
+        token(',').map(|_| Symbol::Comma),
+        token('.').map(|_| Symbol::Dot),
+        token('[').map(|_| Symbol::OpenBracket),
+        token(']').map(|_| Symbol::CloseBracket),
+        token('{').map(|_| Symbol::OpenBrace),
+        token('}').map(|_| Symbol::CloseBrace),
+    ))
+}
