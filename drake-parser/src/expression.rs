d6c7c47a68626fa2db8f22ec66a9af6d3863c913
watcol <potfman@gmail.com>
2022-06-19 18:36:35 +0900

    feat: support arrays and inline tables

--- drake-parser/src/expression.rs
+++ drake-parser/src/expression.rs
@@ -2,18 +2,57 @@
 #[cfg(test)]
 mod tests;
 
-use drake_types::ast::{Expression, ExpressionKind};
-use drake_types::token::Token;
-use somen::prelude::*;
+use alloc::vec::Vec;
+use drake_types::ast::{Expression, ExpressionKind, Key};
+use drake_types::token::{Symbol, Token};
+use somen::{call, prelude::*};
 
-use crate::token::literal;
+use crate::key::key;
+use crate::token::{literal, spaces, symbol};
 
 /// A parser for expression
 pub fn expression<'a, I>() -> impl Parser<I, Output = Expression<I::Locator>> + 'a
 where
     I: Input<Ok = Token> + 'a,
 {
-    choice((literal().map(ExpressionKind::Literal),))
-        .with_position()
-        .map(|(kind, span)| Expression { kind, span })
+    choice((
+        literal().map(ExpressionKind::Literal),
+        array().map(ExpressionKind::Array),
+        inline_table().map(ExpressionKind::InlineTable),
+    ))
+    .with_position()
+    .map(|(kind, span)| Expression { kind, span })
+}
+
+/// A parser for arrays
+pub fn array<'a, I>() -> impl Parser<I, Output = Vec<Expression<I::Locator>>> + 'a
+where
+    I: Input<Ok = Token> + 'a,
+{
+    call!(expression)
+        .skip(spaces(true))
+        .sep_by_end(symbol(Symbol::Comma).skip(spaces(true)), ..)
+        .between(
+            symbol(Symbol::OpenBracket).skip(spaces(true)),
+            symbol(Symbol::CloseBracket),
+        )
+        .collect()
+}
+
+/// A parser for inline tables
+pub fn inline_table<'a, I>(
+) -> impl Parser<I, Output = Vec<(Key<I::Locator>, Expression<I::Locator>)>> + 'a
+where
+    I: Input<Ok = Token> + 'a,
+{
+    key()
+        .skip((spaces(true), symbol(Symbol::Assign), spaces(true)))
+        .and(call!(expression))
+        .skip(spaces(true))
+        .sep_by_end(symbol(Symbol::Comma).skip(spaces(true)), ..)
+        .between(
+            symbol(Symbol::OpenBrace).skip(spaces(true)),
+            symbol(Symbol::CloseBrace),
+        )
+        .collect()
 }
--- drake-parser/src/expression/tests.rs
+++ drake-parser/src/expression/tests.rs
@@ -1,5 +1,7 @@
-use drake_types::ast::{Expression, ExpressionKind, Literal};
-use drake_types::token::{Literal as TokenLit, Token};
+use alloc::string::String;
+use alloc::vec;
+use drake_types::ast::{Expression, ExpressionKind, Key, KeyKind, Literal};
+use drake_types::token::{Identifier, IdentifierKind, Literal as TokenLit, Symbol, Token};
 use somen::prelude::*;
 
 use crate::test_utils::test_parser;
@@ -15,6 +17,266 @@ fn expression() {
                     kind: ExpressionKind::Literal(Literal::Character('a')),
                     span: 0..1,
                 }),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                Some(Expression {
+                    kind: ExpressionKind::Array(vec![]),
+                    span: 0..2,
+                }),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                Some(Expression {
+                    kind: ExpressionKind::InlineTable(vec![]),
+                    span: 0..2,
+                }),
+            ),
+            (&[Token::Whitespaces], None),
+        ],
+    );
+}
+
+#[test]
+fn array() {
+    test_parser(
+        super::array().complete(),
+        &[
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                Some(vec![]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                Some(vec![]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                Some(vec![Expression {
+                    kind: ExpressionKind::Literal(Literal::Character('a')),
+                    span: 1..2,
+                }]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                Some(vec![Expression {
+                    kind: ExpressionKind::Literal(Literal::Character('a')),
+                    span: 1..2,
+                }]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Literal(TokenLit::Character('b')),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                Some(vec![
+                    Expression {
+                        kind: ExpressionKind::Literal(Literal::Character('a')),
+                        span: 1..2,
+                    },
+                    Expression {
+                        kind: ExpressionKind::Literal(Literal::Character('b')),
+                        span: 3..4,
+                    },
+                ]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Literal(TokenLit::Character('b')),
+                    Token::Symbol(Symbol::CloseBracket),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Whitespaces,
+                    Token::Symbol(Symbol::OpenBracket),
+                    Token::Whitespaces,
+                    Token::Symbol(Symbol::CloseBracket),
+                    Token::Newline,
+                ],
+                None,
+            ),
+            (&[Token::Whitespaces], None),
+        ],
+    );
+}
+
+#[test]
+fn inline_table() {
+    test_parser(
+        super::inline_table().complete(),
+        &[
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                Some(vec![]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                Some(vec![]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Identifier(Identifier {
+                        kind: IdentifierKind::Bare,
+                        name: String::from("abc"),
+                    }),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::Assign),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::Comma),
+                    Token::Whitespaces,
+                    Token::Newline,
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                Some(vec![(
+                    Key {
+                        kind: KeyKind::Normal,
+                        name: String::from("abc"),
+                        span: 1..2,
+                    },
+                    Expression {
+                        kind: ExpressionKind::Literal(Literal::Character('a')),
+                        span: 7..8,
+                    },
+                )]),
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Identifier(Identifier {
+                        kind: IdentifierKind::Bare,
+                        name: String::from("abc"),
+                    }),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Identifier(Identifier {
+                        kind: IdentifierKind::Bare,
+                        name: String::from("abc"),
+                    }),
+                    Token::Symbol(Symbol::Assign),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::Comma),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Identifier(Identifier {
+                        kind: IdentifierKind::Bare,
+                        name: String::from("abc"),
+                    }),
+                    Token::Symbol(Symbol::Assign),
+                    Token::Literal(TokenLit::Character('a')),
+                    Token::Identifier(Identifier {
+                        kind: IdentifierKind::Bare,
+                        name: String::from("def"),
+                    }),
+                    Token::Symbol(Symbol::Assign),
+                    Token::Literal(TokenLit::Character('b')),
+                    Token::Symbol(Symbol::CloseBrace),
+                ],
+                None,
+            ),
+            (
+                &[
+                    Token::Whitespaces,
+                    Token::Symbol(Symbol::OpenBrace),
+                    Token::Whitespaces,
+                    Token::Symbol(Symbol::CloseBrace),
+                    Token::Newline,
+                ],
+                None,
             ),
             (&[Token::Whitespaces], None),
         ],
